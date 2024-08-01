9e0407ccf0b04948230f177375cace06daa45915
zsjzyy <2551460239@qq.com>
2024-08-01 16:05:57 +0800

    这是一个测试

---
+++ src/Hello.java
@@ -0,0 +1,19 @@
+import java.sql.SQLOutput;
+
+/**
+ * @author 张士举
+ * @version 1.0
+ */
+public class Hello {
+    public static void main(String[] args) {
+
+        System.out.println("100"+98);
+        System.out.println("-------------");
+        System.out.println(100+98);
+        System.out.println("------------");
+        System.out.println(100+3+"hello");
+        System.out.println("-------------");
+        System.out.println("hello"+100+3);
+    }
+}
+
